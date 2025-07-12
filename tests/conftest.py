33a1b6c179f71f2d05872ffab09140de0dab8870
Александр Лысинский <sanek51532@gmail.com>
2025-07-12 16:11:29 +0400

    Common root commit

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
+from pages import MainPage
+
+
+@pytest.fixture
+def browser():
+    # Настройка Edge WebDriver
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-notifications")
+
+    # Инициализация драйвера
+    driver = webdriver.Edge(service=Service(), options=options)
+    yield driver
+
+    # Закрытие браузера после теста
+    driver.quit()
+
+
+@pytest.fixture
+def main_page(browser):
+    return MainPage(browser)
